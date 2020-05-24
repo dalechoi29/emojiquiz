d98714edcc4a38ac7eb3042ee93ec979a1b38e24
sw choi <smileman29@gmail.com>
2020-05-25 00:17:26 +0900

    스크립트 추가하는 중

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,8 +1,24 @@
 $(document).ready(function () {
 
+// Multiple select
     $('.category').click(function() { 
         $(this).toggleClass('active');
     });
-    
-});
 
+// fadeInDown Animation
+    $('.js--wp-1').waypoint(function (direction) {
+        $('.js--wp-1').addClass('animated fadeInDown');
+    });
+
+// Share
+
+
+// Input 
+    $(':input[type="submit"]').prop('disabled', true);
+    $('input[type="text"]').keyup(function() {
+    if($(this).val() != '') {
+        $(':input[type="submit"]').prop('disabled', false);
+    }
+    });
+
+});
