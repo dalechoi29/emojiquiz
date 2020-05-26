f34a8c479b3600b32c6f32d08361180fb8c569d4
sw choi <smileman29@gmail.com>
2020-05-26 20:59:29 +0900

    share navigator 넣기(btn-share에만 넣어둠) - 핸드폰 chrome과 safari에서만 작동함

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,10 +1,5 @@
 $(document).ready(function () {
 
-// Multiple select
-    $('.category').click(function() { 
-        $(this).toggleClass('active');
-    });
-
 // fadeInDown Animation(index)
     $('.js--ani-1').ready(function (direction) {
         $('.js--ani-1').addClass('animated fadeIn');
@@ -15,9 +10,34 @@ $(document).ready(function () {
         $('.js--ani-2').addClass('animated bounceIn');
     });
 
+// Multiple select(random quiz setting)
+    $('.category').click(function() { 
+        $(this).toggleClass('active');
+    });
+
+
 // Share
 
+function share() {
+    if (navigator.share) {
+        navigator.share({
+                title: 'Emoji Quiz',
+                text: 'Try fun emoji quiz',
+                url: 'https://emoji-quiz.app',
+            })
+            .then(() => console.log('Emoji shared successfully'))
+            .catch((error) => console.log('Error sharing', error));
+    } else {
+        console.log("Your web browser doesn't support share.");
+        alert("Your web browser doesn't support share.")
+    }
+}  
 
+$('a.btn-share').click(function() {
+    share();
+});
+
+  
 // Input 
     // $(':input[type="submit"]').prop('disabled', true);
     // $('input[type="text"]').keyup(function() {
