35e815a9593de308d9506b92e03fcd6855e30fdc
sw choi <smileman29@gmail.com>
2020-05-26 19:59:23 +0900

    애니메이션 추가 및 multi-select

--- resources/js/script.js
+++ resources/js/script.js
@@ -5,20 +5,25 @@ $(document).ready(function () {
         $(this).toggleClass('active');
     });
 
-// fadeInDown Animation
-    $('.js--wp-1').ready(function (direction) {
-        $('.js--wp-1').addClass('animated fadeInDown');
+// fadeInDown Animation(index)
+    $('.js--ani-1').ready(function (direction) {
+        $('.js--ani-1').addClass('animated fadeIn');
+    });
+
+// bounce-in(quiz page)
+    $('.js--ani-2').ready(function (direction) {
+        $('.js--ani-2').addClass('animated bounceIn');
     });
 
 // Share
 
 
 // Input 
-    $(':input[type="submit"]').prop('disabled', true);
-    $('input[type="text"]').keyup(function() {
-    if($(this).val() != '') {
-        $(':input[type="submit"]').prop('disabled', false);
-    }
-    });
+    // $(':input[type="submit"]').prop('disabled', true);
+    // $('input[type="text"]').keyup(function() {
+    // if($(this).val() != '') {
+    //     $(':input[type="submit"]').prop('disabled', false);
+    // }
+    // });
 
 });
